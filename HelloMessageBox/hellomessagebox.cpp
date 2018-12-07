9bfe3e3269bb4f15866f56db376a4edc5e21c22f
lu peng fei <lpfaivc@gmail.com>
2018-12-07 09:22:24 +0800

    up

---
+++ HelloMessageBox/hellomessagebox.cpp
@@ -0,0 +1,10 @@
+
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR szCmdLine, int iCmdShow)
+{
+	MessageBox(NULL, TEXT("Hello, Windows 10!"), TEXT("HelloMsg"), 0);
+
+	return 0;
+}
